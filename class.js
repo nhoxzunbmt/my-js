86fec51e779b1e1f38eabc5615d1b68940b79948
Lợi Trần Thanh <thanhloi.it93@gmail.com>
2018-12-23 23:08:34 +0700

    u

---
+++ class.js
@@ -0,0 +1,62 @@
+function getJson(json) {
+  this.json = json;
+}
+
+getJson.prototype.getAll = function () {
+  console.log(this.json);
+};
+
+var newJson = new getJson("{ a: 1}");
+
+newJson.getAll();
+
+//C2
+
+
+
+class iJson {
+  constructor(json) {
+    this.json = json;
+  }
+  getAll() {
+    console.log(this.json);
+  }
+}
+
+var a = new iJson("{b : 2}");
+a.getAll();
+
+
+class iParent {
+  constructor(json) {
+    this.json = json;
+    console.log('iParent ...');
+  }
+}
+
+
+class iChild extends iParent {
+  constructor(json) {
+    super(json);
+    console.log('iChild ...', this.json);
+
+  }
+  get All() {
+    console.log('getAll ichild');
+    return this.json
+  }
+
+  set Data(json) {
+    this.json = json
+  }
+
+  static printData(){
+    return 'all data'
+  }
+}
+
+var aI = new iChild('{a:1}')
+console.log(iChild.printData());
+// aI.All()
+// aI.Data('{b : 3}')
+// aI.All()
